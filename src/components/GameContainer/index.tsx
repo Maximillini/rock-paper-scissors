853de1c2751a78c441a885bb52e044591d64ee53
Max Iniguez <maximillini86@gmail.com>
2024-05-27 14:23:09 -0500

    Add background gradient, GameContainer component

---
+++ src/components/GameContainer/index.tsx
@@ -0,0 +1,3 @@
+export const GameContainer = () => {
+  return <div className="game-container"></div>
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,10 @@ export const Header = () => {
   return (
     <div className="header-container">
       <img src={logo} />
+      <div className="score-container">
+        <div>Score</div>
+        <div>0</div>
+      </div>
     </div>
   )
 }
