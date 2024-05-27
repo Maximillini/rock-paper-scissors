07dc65e9732ca5755c99b571136197dba8353cf8
Max Iniguez <maximillini86@gmail.com>
2024-05-27 13:58:57 -0500

    Add absolute path config, Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/images/logo.svg'
+import './styles.scss'
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <img src={logo} />
+    </div>
+  )
+}
