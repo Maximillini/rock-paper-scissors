8625433435ef69a918764baddbb93684392772e7
Max Iniguez <maximillini86@gmail.com>
2024-05-29 12:31:52 -0500

    Finalize hand selection layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Header } from './components/Header/index'
+import { SelectionDisplay } from './components/SelectionDisplay'
 import './App.css'
+import { useGameStore } from './stores/useGameStore'
 
 function App() {
+  const score = useGameStore((state) => state.score)
+  const playerHand = useGameStore((state) => state.playerHand)
+
+  console.log({ playerHand })
+
   return (
     <>
-      <Header />
+      <Header score={score} />
+      {playerHand ? (
+        <div>{playerHand}</div>
+      ) : (
+        <div className="game-container">
+          <SelectionDisplay />
+        </div>
+      )}
     </>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import logo from '../../assets/images/logo.svg'
 import './styles.scss'
 
-export const Header = () => {
+export const Header = ({ score }: { score: number }) => {
   return (
     <div className="header-container">
       <img src={logo} />
       <div className="score-container">
-        <div>Score</div>
-        <div>0</div>
+        <div className="score-header">SCORE</div>
+        <div className="score">{score}</div>
       </div>
     </div>
   )
